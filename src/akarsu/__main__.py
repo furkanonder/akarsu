483257467d7ba19c864f73fccadddd91b6852783
furkanonder <furkanonder@protonmail.com>
2023-10-12 02:16:10 +0300

    add new argument to show only the function calls

--- src/akarsu/__main__.py
+++ src/akarsu/__main__.py
@@ -1,8 +1,11 @@
 import argparse
 import io
 from collections import Counter
+from typing import Final
 
 from akarsu.akarsu import Akarsu
+
+CALL_EVENTS: Final[list[str]] = ["C_CALL", "PY_CALL"]
 
 
 def main() -> None:
@@ -11,6 +14,13 @@ def main() -> None:
     )
     parser.add_argument("-v", "--version", action="version", version="0.1.0")
     parser.add_argument("-f", "--file", type=str, help="Path to the file")
+    parser.add_argument(
+        "-c",
+        "--calls",
+        action="store_true",
+        default=False,
+        help="Show only the function calls",
+    )
     args = parser.parse_args()
 
     if file := args.file:
@@ -23,7 +33,12 @@ def main() -> None:
         for event, count in Counter(events).most_common():
             event_type, file_name, func_name = event
             counter[event_type] += count
-            print(f"{count:>10}{event_type:^20}{f'{file_name}({func_name})':<50}")
+            fmt = f"{count:>10}{event_type:^20}{f'{file_name}({func_name})':<50}"
+            if args.calls:
+                if event_type in CALL_EVENTS:
+                    print(fmt)
+            else:
+                print(fmt)
 
         print(f"\nTotal number of events: {counter.total()}")
         for event_type, count in counter.most_common():
