d7bef92a5a6897bae98d7380ac5193659f8c019c
furkanonder <furkanonder@protonmail.com>
2023-10-12 22:14:07 +0300

    handle file not found error

--- src/akarsu/__main__.py
+++ src/akarsu/__main__.py
@@ -1,4 +1,5 @@
 import argparse
+import sys
 from collections import Counter
 from typing import Final
 
@@ -23,8 +24,12 @@ def main() -> None:
     args = parser.parse_args()
 
     if file := args.file:
-        with open(file) as fp:
-            source = fp.read()
+        try:
+            with open(file) as fp:
+                source = fp.read()
+        except FileNotFoundError:
+            print(f"{file} not found.")
+            sys.exit(1)
 
         if source := source.strip():
             events = Akarsu(source, args.file).profile()
@@ -33,13 +38,11 @@ def main() -> None:
             print(f"{'Count':>10}{'Event Type':^20}{'Filename(function)':<50}")
             for event, count in Counter(events).most_common():
                 event_type, file_name, func_name = event
-                counter[event_type] += count
-                fmt = f"{count:>10}{event_type:^20}{f'{file_name}({func_name})':<50}"
-                if args.calls:
-                    if event_type in CALL_EVENTS:
-                        print(fmt)
-                else:
-                    print(fmt)
+                if (args.calls and event_type in CALL_EVENTS) or not args.calls:
+                    counter[event_type] += count
+                    print(
+                        f"{count:>10}{event_type:^20}{f'{file_name}({func_name})':<50}"
+                    )
 
             print(f"\nTotal number of events: {counter.total()}")
             for event_type, count in counter.most_common():
